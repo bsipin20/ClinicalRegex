c534f56618694bac8201a322c3aba35418adc2f3
bsipin20 <briansipin@Brians-MacBook-Pro.local>
2019-02-05 15:47:48 -0500

    removing matches lookupg

--- src/extract_values.py
+++ src/extract_values.py
@@ -276,20 +276,15 @@ def _write_csv_output(note_phrase_matches, note_key, output_fname):
 
         if len(note_phrase_match.phrase_matches) > 0:
             extracted_value = note_phrase_match.phrase_matches[0].extracted_value
-        try: 
 
-            note_phrase_match.note_dict['MATCHES'] = matches
-            note_phrase_match.note_dict['EXTRACTED_VALUE'] = extracted_value
-            dict_list.append(note_phrase_match.note_dict)
-
-        except KeyError:
-            print("This line is not working")
-            pdb.set_trace()
+        note_phrase_match.note_dict['MATCHES'] = matches
+        note_phrase_match.note_dict['EXTRACTED_VALUE'] = extracted_value
+        dict_list.append(note_phrase_match.note_dict)
 
 
 
     df = pd.DataFrame(dict_list)
-    df['MATCHES'] = df['MATCHES'].astype('object')
+    #df['MATCHES'] = df['MATCHES'].astype('object')
     df.index = np.arange(0, df.shape[0])
     df = clean_df(df, [RPDR_NOTE_KEYWORD], False)
     df.to_csv(output_fname)
